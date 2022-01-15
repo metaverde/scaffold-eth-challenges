ef4641715b094365bbe43fa39ccc1e894d20597e
metaverde <jennniferleesilves@gmail.com>
2022-01-15 12:05:16 -0800

    token done working on vendor

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -1,25 +1,36 @@
 pragma solidity 0.8.4;
 // SPDX-License-Identifier: MIT
 
-//import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "./YourToken.sol";
 
-contract Vendor {
+contract Vendor is Ownable {
 
-  YourToken yourToken;
+  YourToken public yourToken;
 
-  constructor(address tokenAddress) public {
+  constructor(address tokenAddress) {
     yourToken = YourToken(tokenAddress);
   }
 
+  mapping ( address => uint256 ) public balances;
+  uint256 public tokensPerEth = 777;
+  uint256 tokenBalance = yourToken.balanceOf(address(this));
+  //need to keep track of the balance of yourToken?
 
   // ToDo: create a payable buyTokens() function:
-
+  function buyTokens() public payable {
+    require(msg.value > 0, "RGP: INSUFFICIENT INPUT BALANCE, YOU NEED TO SEND SOME CAKE");
+  }
 
   // ToDo: create a withdraw() function that lets the owner withdraw ETH
+  function withdraw() public payable {
 
+  }
 
   // ToDo: create a sellTokens() function:
+  //need to make sure there's enough eth in the contract?
+  function sellTokens() public payable {
 
+  }
 
 }
