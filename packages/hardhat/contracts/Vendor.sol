0d57c14d2cc2e9d506c1b3874d2e0a093c7852ba
metaverde <jennniferleesilves@gmail.com>
2022-01-19 09:54:27 -0800

    uncommented deploy scripts etc

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -1,22 +1,48 @@
 pragma solidity 0.8.4;
 // SPDX-License-Identifier: MIT
 
-//import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "./YourToken.sol";
 
-contract Vendor {
+contract Vendor is Ownable {
 
   YourToken yourToken;
+  uint256 public tokensPerEth = 100;
 
-  constructor(address tokenAddress) public {
+  constructor(address tokenAddress) {
     yourToken = YourToken(tokenAddress);
   }
 
-
+  mapping( address => uint256) public balances;
+  //event sent yourToken to msg.sender RENAME TO BoughtTokens
+  event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountToBuy);
+  
   // ToDo: create a payable buyTokens() function:
+  //give somebody money -= my balance and 
+  //get something back.
+  function buyTokens() public payable returns (uint256 tokenAmount) {
+    require(msg.value > 0, "Send ETH to buy some tokens");
+
+    uint256 amountToBuy = msg.value * tokensPerEth;
+
+    // check if the Vendor Contract has enough amount of tokens for the transaction
+    uint256 vendorBalance = yourToken.balanceOf(address(this));
+    require(vendorBalance >= amountToBuy, "Vendor contract has not enough tokens in its balance");
+
+    // Transfer token to the msg.sender
+    (bool sent) = yourToken.transfer(msg.sender, amountToBuy);
+    require(sent, "Failed to transfer token to user");
+
+    // emit the event
+    emit BuyTokens(msg.sender, msg.value, amountToBuy);
+
+    return amountToBuy;
+  }
+
 
 
   // ToDo: create a withdraw() function that lets the owner withdraw ETH
+  
 
 
   // ToDo: create a sellTokens() function:
--- packages/hardhat/contracts/YourToken.sol
+++ packages/hardhat/contracts/YourToken.sol
@@ -3,12 +3,15 @@ pragma solidity 0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 // learn more: https://docs.openzeppelin.com/contracts/3.x/erc20
+//https://docs.openzeppelin.com/contracts/4.x/api/token/erc20#ERC20
+//https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol
 
 contract YourToken is ERC20 {
     // ToDo: add constructor and mint tokens for deployer,
     //       you can use the above import for ERC20.sol. Read the docs ^^^
 
-     constructor() public ERC20("Gold", "GLD") {
-        // _mint() 1000 * 10 ** 18 to msg.sender
+    constructor() ERC20("Gold", "GLD") {
+        _mint(msg.sender, 1000 * (10 ** 18));
+        
     }
 }
